3cb969c523f6f9f9fa8c0b9701014d72a068d987
Jan Priessnitz <jan.priessnitz@gooddata.com>
2018-04-30 18:08:23 +0200

    DO NOT MERGE: Test if raising waitUntilSettled timeout fixes the tests

--- tests/base.py
+++ tests/base.py
@@ -1714,7 +1714,7 @@ class ZuulTestCase(BaseTestCase):
         self.log.debug("Waiting until settled...")
         start = time.time()
         while True:
-            if time.time() - start > 10:
+            if time.time() - start > 40:
                 self.log.debug("Queue status:")
                 for queue in self.event_queues:
                     self.log.debug("  %s: %s" % (queue, queue.empty()))
